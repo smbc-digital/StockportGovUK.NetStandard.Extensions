eb03bd518562ac91764844aee121da5c53a46e93
Elliott J. Humphreys <elliott.humphreys@stockport.gov.uk>
2020-07-15 08:17:17 +0100

    feature(UnitTests): add unit test project, add tests command to workflow

---
+++ UnitTests/VerintExtensions/VerintOnlineFormsExtensions/ConfirmIntegrationEFormExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using StockportGovUK.NetStandard.Extensions.VerintExtensions.VerintOnlineFormsExtensions;
+using StockportGovUK.NetStandard.Models.Verint;
+using Xunit;
+
+namespace UnitTests.VerintExtensions.VerintOnlineFormsExtensions
+{
+    public class ConfirmIntegrationEFormExtensionTests
+    {
+        [Fact]
+        public void ToConfirmIntegrationEFormCase_ShouldReturnPopulatedVerintOnlineFormRequest()
+        {
+            var caseLove = new Case
+            {
+                Customer = new Customer()
+            };
+
+            var configuration = new ConfirmIntegrationEFormExtension.ConfirmIntegrationEFormConfiguration
+            {
+                EventId = 1
+            };
+
+            var result = caseLove.ToConfirmIntegrationEFormCase(configuration);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result.FormData);
+            Assert.Equal("confirm_integrationform", result.FormName);
+        }
+    }
+}
