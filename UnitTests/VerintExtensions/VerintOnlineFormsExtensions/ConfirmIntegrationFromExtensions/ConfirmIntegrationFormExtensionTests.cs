b0d263883691fdb9e066e30476c431db13837b9e
elliotthumphreys <41908900+elliotthumphreys@users.noreply.github.com>
2020-07-16 11:49:37 +0100

    Confrim integration form extensions (#8)
    * fix(ConfirmIntegrationFormExtension): fix address, fix naming
    
    * chore(csproj) version inc

---
+++ UnitTests/VerintExtensions/VerintOnlineFormsExtensions/ConfirmIntegrationFromExtensions/ConfirmIntegrationFormExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using StockportGovUK.NetStandard.Extensions.VerintExtensions.VerintOnlineFormsExtensions.ConfirmIntegrationFromExtensions;
+using StockportGovUK.NetStandard.Models.Verint;
+using Xunit;
+
+namespace UnitTests.VerintExtensions.VerintOnlineFormsExtensions.ConfirmIntegrationFromExtensions
+{
+    public class ConfirmIntegrationFormExtensionTests
+    {
+        [Fact]
+        public void ToConfirmIntegrationFormCase_ShouldReturnPopulatedVerintOnlineFormRequest()
+        {
+            var caseLove = new Case
+            {
+                Customer = new Customer()
+            };
+
+            var configuration = new ConfirmIntegrationFormOptions
+            {
+                EventId = 1
+            };
+
+            var result = caseLove.ToConfirmIntegrationFormCase(configuration);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result.FormData);
+            Assert.Equal("confirm_integrationform", result.FormName);
+        }
+    }
+}
--- src/VerintExtensions/VerintOnlineFormsExtensions/ConfirmIntegrationFromExtensions/ConfirmIntegrationFormOptions.cs
+++ src/VerintExtensions/VerintOnlineFormsExtensions/ConfirmIntegrationFromExtensions/ConfirmIntegrationFormOptions.cs
@@ -1,11 +1,11 @@
-﻿namespace StockportGovUK.NetStandard.Extensions.VerintExtensions.VerintOnlineFormsExtensions.ConfirmIntegrationEFromExtensions
+﻿namespace StockportGovUK.NetStandard.Extensions.VerintExtensions.VerintOnlineFormsExtensions.ConfirmIntegrationFromExtensions
 {
     /// <summary>
     /// Object that's only use will be for providing required information to ToConfirmIntegrationEFormCase extension
     /// method. This object should be populated from the configuration file of a service that wants to use this
     /// extension method.
     /// </summary>
-    public class ConfirmIntegrationEFormOptions
+    public class ConfirmIntegrationFormOptions
     {
         /// <summary>
         /// Used as the objects configurations name in config and startup of projects
@@ -15,6 +15,7 @@
         public string SubjectCode { get; set; }
         public string FollowUp { get; set; }
         public string ClassCode { get; set; }
+        public string ConfirmClassification { get; set; }
         public int EventId { get; set; }
     }
 }
